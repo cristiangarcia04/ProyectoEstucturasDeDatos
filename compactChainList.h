6a0ac17704aa4fa9ff89782f1ac7d5036149442f
Cristiangarcia04 <73946418+cristiangarcia04@users.noreply.github.com>
2025-11-04 15:07:48 -0500

    avance 1

--- compactChainList.h
+++ compactChainList.h
@@ -1,12 +1,49 @@
-#ifndef "PROYECTO_H"
-#define "PROYECTO_H"
+#ifndef COMPACTCHAINLIST_H
+#define COMPACTCHAINLIST_H
 
+#include <vector>
 #include <list>
+#include <utility>
+using namespace std;
+typedef char Element;
 
 class CompactChainList {
     private:
-    element e;
+        vector<pair<Element, int>> CCL;
+        int CCLsize = 0;
+
     public:
-    list();
-    
-}
+        CompactChainList();
+        CompactChainList(vector<Element> &v);
+        CompactChainList(CompactChainList &c);
+
+        int size();
+        int searchElement(Element &e);
+        int getConsecutiveOcurrences(vector<Element> &v);
+        int getIndexFirstConsecutiveOcurrence(vector<Element> &v);
+        int getOcurrences(vector<Element> &v);
+        int getIndexFirstOcurrence(vector<Element> &v);
+        CompactChainList getLexicographicFusion(CompactChainList &c);
+        list<Element> expand();
+
+        bool operator<(const CompactChainList &oth) const;
+        bool operator==(const CompactChainList &oth) const;
+        bool operator==(const Element &oth) const;
+        bool operator==(const int &oth) const;
+        Element operator[](const int pos) const;
+        Element operator[](const int pos);
+        CompactChainList operator+(const CompactChainList &oth) const;
+
+        void set(int &pos, Element &e);
+        void removeFirstOcurrence(Element &e);
+        void removeAllOcurrences(Element &e);
+        void removeBlockPosition(int &pos);
+        void insertElement(int &pos, Element &e);
+        void modifyAllOcurrences(Element &one, Element &two);
+        void push_front(Element &e, int &num);
+        void push_back(Element &e, int &num);
+        static void sortVectorCCL(vector<CompactChainList> &v);
+        void combineEquals();
+};
+
+#endif
