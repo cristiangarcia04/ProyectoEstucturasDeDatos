b68a2e2a2c92afa554b8e69855b82b389c51e52b
Cristiangarcia04 <73946418+cristiangarcia04@users.noreply.github.com>
2025-11-04 15:08:27 -0500

    avance 1

--- compactChainList.cpp
+++ compactChainList.cpp
@@ -1,1 +1,91 @@
-#ifndef ""
+#include "CompactChainList.h"
+
+
+CompactChainList::CompactChainList(){
+}
+
+
+CompactChainList::CompactChainList(vector<Element> &v){
+    CCLsize = v.size();
+
+    if(CCLsize != 0){
+        int count;
+        Element e;
+
+        for(int i = 0; i < CCLsize; i++){
+            if(i == 0){
+                e = v[i];
+                count = 1;
+            }
+            else if(v[i] == e){
+                count +=1;
+            }
+            else {
+                CCL.push_back(pair{e,count});
+                e = v[i];
+                count = 1;
+            }
+        }
+        CCL.push_back(pair{e,count});
+    }
+}
+
+
+CompactChainList::CompactChainList(CompactChainList &c){
+    CCL = c.CCL;
+    CCLsize = c.CCLsize;
+}
+
+
+int CompactChainList::size(){
+    return CCLsize;
+}
+
+
+int CompactChainList::searchElement(Element &e){
+    int pos = 0;
+    bool flag = false;
+
+    for(int i = 0; i < CCL.size() && !flag; i++){
+        if(CCL[i].first != e){
+            pos += CCL[i].second;
+        }
+        else{
+            flag = true;
+        }
+    }
+    if(flag == false){
+        pos = -1;
+    }
+    return pos;
+}
+
+
+
+
+void CompactChainList::push_front(Element &e, int &num){
+    if(CCL.empty()){
+        CCL.push_back(pair{e, num});
+    }
+    else if(CCL[0].first == e){
+            CCL[0].second += num;
+    }
+    else{
+        CCL.insert(CCL.begin(), pair{e,num});
+    }
+    CCLsize += num;
+}
+
+
+void CompactChainList::push_back(Element &e, int &num){
+    if(CCL.empty()){
+        CCL.push_back(pair{e, num});
+    }
+    else if(CCL[CCL.size()-1].first == e){
+            CCL[CCL.size()-1].second += num;
+    }
+    else{
+        CCL.push_back({e,num});
+    }
+    CCLsize += num;
+}
